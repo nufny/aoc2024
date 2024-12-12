1c8b2eaad84c67aa3d32fc57608b93bc031aa8e0
nufny <nufny@zweitekassabitte.jetzt>
2024-12-12 16:43:41 +0100

    fix templates

--- templates/test_template.rs
+++ templates/test_template.rs
@@ -1,10 +1,11 @@
 
+use super::dayx;
 // Day x tests
 #[test]
 fn dayxp1() {
-    default_test(x,1,|input| dayx::p1::out(input).to_string());
+    default_test(x,1,|input| dayx::p1::run(input).to_string());
 }
 #[test]
 fn dayxp2() {
-    default_test(x,2,|input| dayx::p2::out(input).to_string());
+    default_test(x,2,|input| dayx::p2::run(input).to_string());
 }
