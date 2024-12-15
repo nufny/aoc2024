8ab61dd090845ee425d47cff94a79a5e825a3c58
nufny <nufny@zweitekassabitte.jetzt>
2024-12-15 22:54:37 +0100

    update template so compiler screams less at me

--- templates/day_template.rs
+++ templates/day_template.rs
@@ -1,11 +1,11 @@
 pub mod p1 {
-    pub fn run(input: String) -> u32 {
+    pub fn run(_input: String) -> u32 {
         0
     }
 }
 
 pub mod p2 {
-    pub fn run(input: String) -> u32 {
+    pub fn run(_input: String) -> u32 {
         0
     }
 }
